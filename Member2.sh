8414ee907c52d7d7a8745ee75c551c592293e56c
Y0lk4 <143545450+Y0lk4@users.noreply.github.com>
2024-11-13 11:11:47 -0500

    Member2 Jonathan
    backups(in progress) and service management

---
+++ Member2.sh
@@ -0,0 +1,89 @@
+#Backups
+
+read -p " Enter the file name you would like to backup: " filename
+read -p " Enter the date for the backup: " date
+read -p " Enter the time for the backup: " Time
+	backup_file="${filename}_backup_${date} ${Time}.bak"
+	 # Check if file exists
+    if [ ! -f "$filename ] then
+	echo " File '$filename' doesn't exist. "
+    else
+   	cp "$filename" "$backup_file"
+    fi
+
+    # Check if backup folder exists, if not, create it
+    if [ -e BackupFiles ] then
+    	mv $backup_file BackupFiles
+    else
+        mkdir BackupFiles
+	mv $backup_file BackupFiles
+    fi
+
+
+    #When user wants to see the last time the backup process was used
+    LatestBackupDate="${date} ${Time}"
+
+
+
+#Service management
+if [ ! -f services.txt ]; then
+touch services.txt
+fi
+echo " Would you like to list all the services? "
+
+select option in Yes No
+do
+	case($option) in
+	Yes)
+		systemctl --type=service > services.txt
+		cat services.txt
+		read -p " Which service would you like to start or stop?: " service
+		echo " Would you like to stop or start the service? "
+
+		select StartStop in Stop Start
+		do
+			case($StartStop) in
+			Stop)
+				echo " Stopping service ${service}... "
+				sudo systemctl stop $service
+				echo " Service $Service stopped. "
+				;;
+			Start)
+				echo " Starting service ${service}... "
+				sudo systemctl start $service
+				echo " Service $service started. "
+				;;
+			*)
+				echo " Invalid input. "
+				;;
+			esac
+		done
+	No)
+		read -p " Which service would you like to start or stop?: " service
+		echo " Would you like to stop or start the service? "
+
+		select StartStop in Stop Start
+                do
+                        case($StartStop) in
+                        Stop)
+                                echo " Stopping service ${service}... "
+                                sudo systemctl stop $service
+                                echo " Service $Service stopped. "
+                                ;;
+                        Start)
+                                echo " Starting service ${service}... "
+                                sudo systemctl start $service
+                                echo " Service $service started. "
+                                ;;
+			*)
+				echo " Invalid input. "
+				;;
+                        esac
+                done
+	*)
+		echo " Invalid input. "
+
+
+
+
+
